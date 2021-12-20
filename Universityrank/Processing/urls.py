15760cbc830b487f4ce4eed7719f8728ad333809
Nikhil Salvi <78545675+NikhilSalvi12@users.noreply.github.com>
2021-12-21 00:21:51 +0530

    Delete Universityrank directory

--- Universityrank/Processing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('',views.userpannel,name="Userpannel"),
-    path('calculate/', views.getrank ,name="CalculateRank"),
-    path('data/',views.data ,name="ReferenceData"),
-]
