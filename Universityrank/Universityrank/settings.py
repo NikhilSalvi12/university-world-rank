b51a72416db6c1bffc45c2e5c50fc2954553a461
nikhilsalvi47@gmail.com <nikhilsalvi47@gmail.com>
2021-12-20 21:50:19 +0530

    Keys hide

--- Universityrank/Universityrank/settings.py
+++ Universityrank/Universityrank/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'Universityrank.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME' : BASE_DIR/'db.sqlite3',
+    'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ciba',
     }
 
 }
