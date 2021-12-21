991926ddb2752bb9466c1392900624552508aa48
nikhilsalvi47@gmail.com <nikhilsalvi47@gmail.com>
2021-12-21 21:47:34 +0530

    edited views

--- Processing/views.py
+++ Processing/views.py
@@ -3,13 +3,15 @@ import pickle
 import sklearn
 import pandas as pd
 
+
 # Create your views here.
 
-
+# Passes the homepage
 def userpannel(request):
     return render(request, "index.html",{"Rank":""})
 
 
+#calculate the rank from the predicted values
 def getrank(request):
     Institute_name = request.GET["Institute_name"]
     Country = request.GET["Country"]
@@ -33,15 +35,17 @@ def getrank(request):
                        'citations': citations, 'broad_impact': broad_impact, 'patents': patents,
                        'score': score, 'year': year}])
 
-    norm = pickle.load(open(r'..\sav_files\normalize.sav', 'rb'))
+    norm = pickle.load(open(r'Universityrank\sav_files\normalize.sav', 'rb'))
     df_norm = norm.transform(df)
 
-    model = pickle.load(open(r'..\sav_files\finalized_model.sav', 'rb'))
+    model = pickle.load(open(r'Universityrank\sav_files\finalized_model.sav', 'rb'))
     prediction = model.predict(df_norm)
     prediction = prediction[0].round()
 
     return render(request, "index.html", {"Rank": prediction, "Name": Institute_name})
 
+
+#the reference data file
 def data(request):
     #Ref_data = pd.read_csv(){"CSVFile": r'static\data.csv'}
     return render(request,'Reference_Data.html')
