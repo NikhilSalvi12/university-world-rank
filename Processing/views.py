a7bcfb0ecbb3fc908a3264e14cf510f39d4a87f9
nikhilsalvi47@gmail.com <nikhilsalvi47@gmail.com>
2021-12-21 21:04:09 +0530

    sav_files directory

--- Processing/views.py
+++ Processing/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import pickle
 import sklearn
 import pandas as pd
+
 # Create your views here.
 
 
@@ -32,10 +33,10 @@ def getrank(request):
                        'citations': citations, 'broad_impact': broad_impact, 'patents': patents,
                        'score': score, 'year': year}])
 
-    norm = pickle.load(open(r'static\normalize.sav', 'rb'))
+    norm = pickle.load(open(r'sav_files\normalize', 'rb'))
     df_norm = norm.transform(df)
 
-    model = pickle.load(open(r'static\finalized_model.sav', 'rb'))
+    model = pickle.load(open(r'sav_files\finalized_model.sav', 'rb'))
     prediction = model.predict(df_norm)
     prediction = prediction[0].round()
 
